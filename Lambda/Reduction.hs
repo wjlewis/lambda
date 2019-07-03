856a82b44d4ff4e53dbb9d2d9cd4ef39caa44369
william j lewis <william.lewis112358@gmail.com>
2019-07-03 13:41:20 -0400

    Add normal-order reduction
    I am not *entirely* sure that this implementation is correct. I need to
    read more about reduction strategies.

---
+++ Lambda/Reduction.hs
@@ -0,0 +1,25 @@
+module Reduction (reduce) where
+
+import Term (NTerm(..), beta)
+
+-- To reduce a term to a normal form (if it has one), we simple apply
+-- the stepping function until it can no longer make any progress.
+reduce :: NTerm -> NTerm
+reduce = loop . pure
+  where loop = either id (loop . step)
+
+-- This function performs a single step of a normal-order reduction to
+-- the given term. If the term is irreducible, it returns the term
+-- wrapped in a `Left`; otherwise, it returns the reduced term in a
+-- `Right`.
+step :: NTerm -> Either NTerm NTerm
+step t@(NVar _ _)          = Left t
+step (NAbs n b)            = NAbs n <$$> step b
+step t@(NApp (NAbs _ _) _) = Right $ beta t
+step (NApp t u)            = case step t of
+                               Left t'  -> NApp t <$$> step u
+                               Right t' -> Right $ NApp t' u
+
+(<$$>) :: (a -> a) -> Either a a -> Either a a
+f <$$> Left x  = Left $ f x
+f <$$> Right x = Right $ f x
